2ef3bb6ab64232b74cc9fea2c476a50fee62d971
dslim <156780850+dongseonglim@users.noreply.github.com>
2024-03-13 16:54:39 +0900

    Update sum_of_divisors.js

--- js/lv1/sum_of_divisors.js
+++ js/lv1/sum_of_divisors.js
@@ -1,15 +1,15 @@
 function solution(n, arr1, arr2) {
     var answer = [];
 
-  	const map1 = arr1.map(arr => {
-  		return Array.from(arr.toString(2).padStart(n,'0'));
-  	})
-  	console.log(map1);
-  
-  	const map2 = arr2.map(arr => {
-  		return Array.from(arr.toString(2).padStart(n,'0'));
-  	})
-  	console.log(map2);
+	const map1 = arr1.map(arr => {
+		return Array.from(arr.toString(2).padStart(n,'0'));
+	})
+	console.log(map1);
+
+	const map2 = arr2.map(arr => {
+		return Array.from(arr.toString(2).padStart(n,'0'));
+	})
+	console.log(map2);
 	
     for (var i=0; i<n; i++) {
         let temp = '';
@@ -24,6 +24,7 @@ function solution(n, arr1, arr2) {
         answer.push(temp);
         temp = '';
     }
+    
 
     return answer;
 }
